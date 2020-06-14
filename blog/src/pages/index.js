9ffb039f09aaf1762e676b384c83094c6fee4938
MouadlGou <mouad.elgouraid@gmail.com>
2020-06-14 16:48:06 +0100

    tutorial_system

---
+++ blog/src/pages/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Link,graphql } from "gatsby"
+import Layout from "../components/layout"
+
+
+const IndexPage = ({data}) => (
+  <Layout>
+   
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+     
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    
+  </Layout>
+)
+
+export default IndexPage
+
+export const pageQuery = graphql`{
+  query IndexQuery {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+          title
+          content
+        }
+      }
+    }
+  }
+}`
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1d4a5834-e678-4731-a371-f3818bb455d4'
+};
